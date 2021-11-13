bd1fabc9488d821be55e6edc6d0bc802d8df0fc8
Samee Arif <23100088@lums.edu.pk>
2021-11-13 21:23:14 +0500

    Regex Constraints Fixed

--- Database/Scripts/DDL.sql
+++ Database/Scripts/DDL.sql
@@ -1,3 +1,5 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
 DROP TABLE IF EXISTS phone;
 DROP TABLE IF EXISTS delivery_radius;
 DROP TABLE IF EXISTS ordered_item;
@@ -8,6 +10,8 @@ DROP TABLE IF EXISTS rider;
 DROP TABLE IF EXISTS credit_card;
 DROP TABLE IF EXISTS vendor;
 DROP TABLE IF EXISTS consumer;
+
+SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE vendor(
     ID INT AUTO_INCREMENT PRIMARY KEY,
@@ -20,19 +24,19 @@ CREATE TABLE vendor(
     bank_account CHAR(24) NOT NULL,
     image_url VARCHAR(255),
     rating NUMERIC(2,1),
-    CHECK (city in ('Islamabad', 'Lahore', 'Karachi', 'Faislabad', 'Sialkot', 'Peshawar', 'Quetta')),
+    CHECK (city in ('Islamabad', 'Lahore', 'Karachi', 'Faisalabad', 'Sialkot', 'Peshawar', 'Quetta')),
     CHECK (area in ('Gulberg', 'Defence', 'Model Town', 'Johar Town', 'Sea View')),
     CHECK (street_num >= 1),
     CHECK (building_num >= 1),
-    CHECK (bank_account like 'PK[0-9][0-9][A-Z][A-Z][A-Z][A-Z][0-9999999999999999]'),
-    CHECK (email_id like('%@%'))
+    CHECK (REGEXP_LIKE(bank_account, 'PK[0-9][0-9][A-Z][A-Z][A-Z][A-Z][0-9999999999999999]')),
+    CHECK (email_id like '%@%')
 );
 
 CREATE TABLE phone(
     phone_num CHAR(11) PRIMARY KEY,
     ID INT NOT NULL,
     FOREIGN KEY (ID) REFERENCES vendor(ID),
-    CHECK (phone_num like '03[0-999999999]')
+    CHECK (REGEXP_LIKE(phone_num, '03[0-999999999]'))
     
 );
 
@@ -41,7 +45,7 @@ CREATE TABLE delivery_radius(
     city VARCHAR(15) NOT NULL,
     area VARCHAR(30) NOT NULL,
     FOREIGN KEY (ID) REFERENCES vendor(ID),
-    CHECK (city in ('Islamabad', 'Lahore', 'Karachi', 'Faislabad', 'Sialkot', 'Peshawar', 'Quetta')),
+    CHECK (city in ('Islamabad', 'Lahore', 'Karachi', 'Faisalabad', 'Sialkot', 'Peshawar', 'Quetta')),
     CHECK (area in ('Gulberg', 'Defence', 'Model Town', 'Johar Town', 'Sea View'))
 );
 
@@ -74,16 +78,16 @@ CREATE TABLE rider(
     gender VARCHAR(6) NOT NULL,
     bank_account CHAR(24) NOT NULL,
     live_location VARCHAR(255),
-    CHECK (city in ('Islamabad', 'Lahore', 'Karachi', 'Faislabad', 'Sialkot', 'Peshawar', 'Quetta')),
+    CHECK (city in ('Islamabad', 'Lahore', 'Karachi', 'Faisalabad', 'Sialkot', 'Peshawar', 'Quetta')),
     CHECK (area in ('Gulberg', 'Defence', 'Model Town', 'Johar Town', 'Sea View')),
     CHECK (street_num >= 1),
     CHECK (building_num >= 1),
     CHECK (vehicle_type in ('Car', 'Motorcycle', 'Bicycle', 'Rickshaw')),
-    CHECK (vehicle_registration_num like '[A-Z]%'),
+    CHECK (REGEXP_LIKE(vehicle_registration_num, '^[A-Z]')),
     CHECK (gender in ('Male', 'Female', 'Other')),
-    CHECK (email_id like('%@%')),
-    CHECK (phone_num like '03[0-999999999]'),
-    CHECK (bank_account like 'PK[0-9][0-9][A-Z][A-Z][A-Z][A-Z][0-9999999999999999]')
+    CHECK (email_id like '%@%'),
+    CHECK (REGEXP_LIKE(phone_num, '03[0-999999999]')),
+    CHECK (REGEXP_LIKE(bank_account, 'PK[0-9][0-9][A-Z][A-Z][A-Z][A-Z][0-9999999999999999]'))
 );
 
 CREATE TABLE consumer(
@@ -99,14 +103,14 @@ CREATE TABLE consumer(
     wallet INT DEFAULT 0 NOT NULL,
     email_id VARCHAR(50) NOT NULL,
     gender VARCHAR(6) NOT NULL,
-    CHECK (city in ('Islamabad', 'Lahore', 'Karachi', 'Faislabad', 'Sialkot', 'Peshawar', 'Quetta')),
+    CHECK (city in ('Islamabad', 'Lahore', 'Karachi', 'Faisalabad', 'Sialkot', 'Peshawar', 'Quetta')),
     CHECK (area in ('Gulberg', 'Defence', 'Model Town', 'Johar Town', 'Sea View')),
     CHECK (street_num >= 1),
     CHECK (building_num >= 1),
     CHECK (gender in ('Male', 'Female', 'Other')),
-    CHECK (phone_num like '03[0-999999999]'),
-    CHECK (email_id like('%@%')),
-    CHECK (wallet >= 1)
+    CHECK (REGEXP_LIKE(phone_num, '03[0-999999999]')),
+    CHECK (email_id like '%@%'),
+    CHECK (wallet >= 0)
 );
 
 CREATE TABLE credit_card(
@@ -118,10 +122,10 @@ CREATE TABLE credit_card(
     type VARCHAR(10) NOT NULL,
     PRIMARY KEY(credit_card_num),
     FOREIGN KEY (ID) REFERENCES consumer(ID),
-    CHECK (cvc like '[0-9][0-9][0-9]'),
-    CHECK (credit_card_num like '[0-999999999999]'),
-    CHECK (expiry_date_month like '[0-99]'),
-    CHECK (expiry_date_year like '[0-9999]'),
+    CHECK (REGEXP_LIKE(cvc, '[0-999]')),
+    CHECK (REGEXP_LIKE(credit_card_num, '[0-999999999999]')),
+    CHECK (REGEXP_LIKE(expiry_date_month, '[0-99]')),
+    CHECK (REGEXP_LIKE(expiry_date_year, '[0-9999]')),
     CHECK (type in ('Visa', 'Master'))
 );
 
@@ -149,7 +153,7 @@ CREATE TABLE ordered_item(
     vendor_ID INT NOT NULL,
     name VARCHAR(30) NOT NULL,
     quantity INT NOT NULL,
-    PRIMARY KEY (orders_ID, vendor_ID),
+    PRIMARY KEY (orders_ID, vendor_ID, name),
     FOREIGN KEY (orders_ID) REFERENCES orders(ID),
     FOREIGN KEY (vendor_ID) REFERENCES vendor(ID),
     CHECK (quantity >= 1)
@@ -159,10 +163,11 @@ CREATE TABLE transaction(
     ID INT AUTO_INCREMENT PRIMARY KEY,
     orders_ID INT NOT NULL,
     payment_method VARCHAR(6) NOT NULL,
-    credit_card_num VARCHAR(16),
-    FOREIGN KEY orders_ID REFERENCES orders(ID),
+    credit_card_num CHAR(12),
+    FOREIGN KEY (orders_ID) REFERENCES orders(ID),
     FOREIGN KEY (credit_card_num) REFERENCES credit_card(credit_card_num),
-    CHECK (payment_method in ('Online', 'Cash'))
+    CHECK (payment_method in ('Online', 'Cash')),
+    CHECK ((payment_method = 'Cash' AND credit_card_num IS NULL) OR (payment_method = 'Online' AND credit_card_num IS NOT NULL))
 );
 
 
--- Database/Scripts/DML.sql
+++ Database/Scripts/DML.sql
@@ -1,13 +1,17 @@
-DROP FROM phone;
-DROP FROM delivery_radius;
-DROP FROM ordered_item;
-DROP FROM item;
-DROP FROM orders;
-DROP FROM transaction;
-DROP FROM rider;
-DROP FROM credit_card;
-DROP FROM vendor;
-DROP FROM consumer;
+SET FOREIGN_KEY_CHECKS = 0;
+
+DELETE FROM phone;
+DELETE FROM delivery_radius;
+DELETE FROM ordered_item;
+DELETE FROM item;
+DELETE FROM orders;
+DELETE FROM transaction;
+DELETE FROM rider;
+DELETE FROM credit_card;
+DELETE FROM vendor;
+DELETE FROM consumer;
+
+SET FOREIGN_KEY_CHECKS = 1;
 
 INSERT INTO vendor(ID, name, city, area, street_num, building_num, email_id, bank_account, image_url, rating) VALUES (1, "KFC", "Lahore", "Defence", 1, 88, "KFC@gmail.com", "PK11MEZN1234567891234567", NULL, 4.5);
 INSERT INTO vendor(ID, name, city, area, street_num, building_num, email_id, bank_account, image_url, rating) VALUES (NULL, "McDonald's", "Karachi", "Gulberg", 3, 144, "McDonald@gmail.com", "PK22MEZN1434517891234567", NULL, 5);
@@ -17,35 +21,35 @@ INSERT INTO vendor(ID, name, city, area, street_num, building_num, email_id, ban
 
 INSERT INTO consumer(ID, first_name, middle_name, last_name, city, area, street_num, building_num, phone_num, wallet, email_id, gender) VALUES (1, "Mohammad", "Taha", "Zakir", "Karachi", "Sea View", 42, 2, "03234567891", 500, "tahazakir@gmail.com", "Male");
 INSERT INTO consumer(ID, first_name, middle_name, last_name, city, area, street_num, building_num, phone_num, wallet, email_id, gender) VALUES (NULL, "Muhammad", NULL, "Taha", "Lahore", "Gulberg", 321, 52, "03241567891", 0, "muhammadtaha@gmail.com", "Female");
-INSERT INTO consumer(ID, first_name, middle_name, last_name, city, area, street_num, building_num, phone_num, wallet, email_id, gender) VALUES (NULL, "Hamza", NULL, "Rafi", "Quetta", "Defense", 111, 1, "03234565811", 142, "hamzarafi@gmail.com", "Other");
+INSERT INTO consumer(ID, first_name, middle_name, last_name, city, area, street_num, building_num, phone_num, wallet, email_id, gender) VALUES (NULL, "Hamza", NULL, "Rafi", "Quetta", "Defence", 111, 1, "03234565811", 142, "hamzarafi@gmail.com", "Other");
 INSERT INTO consumer(ID, first_name, middle_name, last_name, city, area, street_num, building_num, phone_num, wallet, email_id, gender) VALUES (NULL, "Syed", "Ali", "Irteza", "Faisalabad", "Model Town", 31, 412, "03224511891", 214, "alii@gmail.com", "Female");
-INSERT INTO consumer(ID, first_name, middle_name, last_name, city, area, street_num, building_num, phone_num, wallet, email_id, gender) VALUES (NULL, "Abdullah", NULL, "Ijaz", "Sialkot", "Defense", 511, 112, "03234564411", 11, "abdullahijz@gmail.com", "Male");
+INSERT INTO consumer(ID, first_name, middle_name, last_name, city, area, street_num, building_num, phone_num, wallet, email_id, gender) VALUES (NULL, "Abdullah", NULL, "Ijaz", "Sialkot", "Defence", 511, 112, "03234564411", 11, "abdullahijz@gmail.com", "Male");
 
-INSERT INTO rider VALUES(1, "Mutahar", NULL, "Ali", "Lahore", "Gulberg", 3, 14, "Car", "LE-8080", "03004958800", "Male", "PK86UBLT5050358099703523", NULL);
-INSERT INTO rider VALUES(NULL, "Faizan", NULL, "Elahi", "Lahore", "Defence", 20, 15, "Motorcycle", "A420", "03008976543", "Male", "PK86JSLT5050358096303523", NULL);
-INSERT INTO rider VALUES(NULL, "Muhammad", "Bilal", "Shahid", "Lahore", "Model Town", 5, 420, "Bicycle", "LQ-69", "03314950730", "Other", "PK89HBLT5050358099709876", NULL);
-INSERT INTO rider VALUES(NULL, "Samee", NULL, "Arif", "Karachi", "Sea view", 3, 24, "Car", "LE-7171", "03244957700", "Male", "PK71UBLT5050058099693523", NULL);
-INSERT INTO rider VALUES(NULL, "Mustafa", NULL, "Sidiqui", "Faislabad", "Defence", 8, 34, "Motorcycle", "LEF-8197", "03004958800", "Male", "PK51FSBL5050358099703523", NULL);
-INSERT INTO rider VALUES(NULL, "Hafsa", NULL, "Khan", "Islamabad", "Defence", 15, 8, "Car", "LEA-8097", "03518875963", "Female", "PK11SKLE5050357799703523", NULL);
+INSERT INTO rider(ID, first_name, middle_name, last_name, city, area, street_num, building_num, vehicle_type, vehicle_registration_num, phone_num, email_id, gender, bank_account, live_location) VALUES(1, "Mutahar", NULL, "Ali", "Lahore", "Gulberg", 3, 14, "Car", "LE-8080", "03004958800", "mutahar@gmail.com" ,"Male", "PK86UBLT5050358099703523", NULL);
+INSERT INTO rider(ID, first_name, middle_name, last_name, city, area, street_num, building_num, vehicle_type, vehicle_registration_num, phone_num, email_id, gender, bank_account, live_location) VALUES(NULL, "Faizan", NULL, "Elahi", "Lahore", "Defence", 20, 15, "Motorcycle", "A420", "03008976543", "faizan@gmail.com", "Male", "PK86JSLT5050358096303523", NULL);
+INSERT INTO rider(ID, first_name, middle_name, last_name, city, area, street_num, building_num, vehicle_type, vehicle_registration_num, phone_num, email_id, gender, bank_account, live_location) VALUES(NULL, "Muhammad", "Bilal", "Shahid", "Lahore", "Model Town", 5, 420, "Bicycle", "LQ-69", "03314950730", "bilal@gmail.com", "Other", "PK89HBLT5050358099709876", NULL);
+INSERT INTO rider(ID, first_name, middle_name, last_name, city, area, street_num, building_num, vehicle_type, vehicle_registration_num, phone_num, email_id, gender, bank_account, live_location) VALUES(NULL, "Samee", NULL, "Arif", "Karachi", "Sea view", 3, 24, "Car", "LE-7171", "03244957700", "samee@gmail.com", "Male", "PK71UBLT5050058099693523", NULL);
+INSERT INTO rider(ID, first_name, middle_name, last_name, city, area, street_num, building_num, vehicle_type, vehicle_registration_num, phone_num, email_id, gender, bank_account, live_location) VALUES(NULL, "Mustafa", NULL, "Sidiqui", "Faisalabad", "Defence", 8, 34, "Motorcycle", "LEF-8197", "03004958800", "mutafa@gmail.com","Male", "PK51FSBL5050358099703523", NULL);
+INSERT INTO rider(ID, first_name, middle_name, last_name, city, area, street_num, building_num, vehicle_type, vehicle_registration_num, phone_num, email_id, gender, bank_account, live_location) VALUES(NULL, "Hafsa", NULL, "Khan", "Islamabad", "Defence", 15, 8, "Car", "LEA-8097", "03518875963", "khan@gmail.com", "Female", "PK11SKLE5050357799703523", NULL);
 
-INSERT INTO orders VALUES(1, 1, 2, 1, "Delivered", 450, 5.0, '2021-04-03 14:00:45', '2021-04-03 14:30:45');
-INSERT INTO orders VALUES(NULL, 3, 2, 3, "Delivered", 650, 4.2, '2021-07-14 17:00:16', '2021-07-14 17:55:45');
-INSERT INTO orders VALUES(NULL, 3, 1, 5, "Delivered", 699, 1.5, '2021-11-03 14:00:45', '2021-11-03 14:30:16');
-INSERT INTO orders VALUES(NULL, 2, 5, 2, "Delivered", 788, 4.0, '2021-10-21 13:35:52', '2021-10-21 14:45:12');
-INSERT INTO orders VALUES(NULL, 4, 4, 2, "Delivered", 1012, 3.5, '2021-04-03 14:00:45', '2021-04-03 14:30:45');
-INSERT INTO orders VALUES(NULL, 5, 3, 2, "Cancelled", 920, 2.5, '2021-05-18 14:16:45', '2021-05-18 14:55:45');
+INSERT INTO orders(ID, vendor_ID, consumer_ID, rider_ID, status, value, rating, placed, delivered) VALUES(1, 1, 2, 1, "Delivered", 450, 5.0, '2021-04-03 14:00:45', '2021-04-03 14:30:45');
+INSERT INTO orders(ID, vendor_ID, consumer_ID, rider_ID, status, value, rating, placed, delivered) VALUES(NULL, 3, 2, 3, "Delivered", 650, 4.2, '2021-07-14 17:00:16', '2021-07-14 17:55:45');
+INSERT INTO orders(ID, vendor_ID, consumer_ID, rider_ID, status, value, rating, placed, delivered) VALUES(NULL, 3, 1, 5, "Delivered", 699, 1.5, '2021-11-03 14:00:45', '2021-11-03 14:30:16');
+INSERT INTO orders(ID, vendor_ID, consumer_ID, rider_ID, status, value, rating, placed, delivered) VALUES(NULL, 2, 5, 2, "Delivered", 788, 4.0, '2021-10-21 13:35:52', '2021-10-21 14:45:12');
+INSERT INTO orders(ID, vendor_ID, consumer_ID, rider_ID, status, value, rating, placed, delivered) VALUES(NULL, 4, 4, 2, "Delivered", 1012, 3.5, '2021-04-03 14:00:45', '2021-04-03 14:30:45');
+INSERT INTO orders(ID, vendor_ID, consumer_ID, rider_ID, status, value, rating, placed, delivered) VALUES(NULL, 5, 3, 2, "Cancelled", 920, 2.5, '2021-05-18 14:16:45', '2021-05-18 14:55:45');
 
-INSERT INTO phone(phone_num, ID) VALUES (1, "03326677133");
-INSERT INTO phone(phone_num, ID) VALUES (2, "03322217133");
-INSERT INTO phone(phone_num, ID) VALUES (3, "03326611533");
-INSERT INTO phone(phone_num, ID) VALUES (4, "02521677133");
-INSERT INTO phone(phone_num, ID) VALUES (5, "03112377133");
+INSERT INTO phone(phone_num, ID) VALUES ("03326677133", 1);
+INSERT INTO phone(phone_num, ID) VALUES ("03322217133", 2);
+INSERT INTO phone(phone_num, ID) VALUES ("03326611533", 3);
+INSERT INTO phone(phone_num, ID) VALUES ("03521677133", 4);
+INSERT INTO phone(phone_num, ID) VALUES ("03112377133", 5);
 
-INSERT INTO delivery_radius(ID, city, area) VALUES (1, "Lahore", "Defense");
+INSERT INTO delivery_radius(ID, city, area) VALUES (1, "Lahore", "Defence");
 INSERT INTO delivery_radius(ID, city, area) VALUES (1, "Lahore", "Gulberg");
 INSERT INTO delivery_radius(ID, city, area) VALUES (2, "Karachi", "Sea View");
 INSERT INTO delivery_radius(ID, city, area) VALUES (3, "Faisalabad", "Johar Town");
-INSERT INTO delivery_radius(ID, city, area) VALUES (4, "Quetta", "Defense");
+INSERT INTO delivery_radius(ID, city, area) VALUES (4, "Quetta", "Defence");
 INSERT INTO delivery_radius(ID, city, area) VALUES (5, "Sialkot", "Model Town");
 
 INSERT INTO item(ID, name, price, category, image_url, description) VALUES (1, "Burger", 500, "Fast Food", NULL, "Good burger");
@@ -69,7 +73,7 @@ INSERT INTO ordered_item(orders_ID, vendor_ID, name, quantity) VALUES (4, 4, "Pa
 INSERT INTO ordered_item(orders_ID, vendor_ID, name, quantity) VALUES (5, 5, "Pizza Xtreme", 2);
 
 INSERT INTO transaction(ID, orders_ID, payment_method, credit_card_num) VALUES (1, 1, "Online", 123456789123);
-INSERT INTO transaction(ID, orders_ID, payment_method, credit_card_num) VALUES (NULL, 2, "Cash", 123451711123);
+INSERT INTO transaction(ID, orders_ID, payment_method, credit_card_num) VALUES (NULL, 2, "Cash", NULL);
 INSERT INTO transaction(ID, orders_ID, payment_method, credit_card_num) VALUES (NULL, 3, "Online", 121222554423);
 INSERT INTO transaction(ID, orders_ID, payment_method, credit_card_num) VALUES (NULL, 4, "Online", 155556781123);
-INSERT INTO transaction(ID, orders_ID, payment_method, credit_card_num) VALUES (NULL, 5, "Cash", 123427666623);
+INSERT INTO transaction(ID, orders_ID, payment_method, credit_card_num) VALUES (NULL, 5, "Cash", NULL);
